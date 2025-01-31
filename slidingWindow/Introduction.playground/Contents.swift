528368d4b1369389e2695202771996e5772ec4d0
Sarvar019 <megafon.kcr.09.09@gmail.com>
2025-01-31 16:05:30 +0500

    introduction to sliding window

--- slidingWindow/Introduction.playground/Contents.swift
+++ slidingWindow/Introduction.playground/Contents.swift
@@ -18,8 +18,32 @@ func findAverageSubArrays(_ nums: [Int], k: Int) -> [Double] {
 }
 
 let result = findAverageSubArrays([1, 3, 2, 6, -1, 4, 1, 8, 2], k: 5)
-print(result)
+print("Average Size: ", result, "First method")
 
+
+// MARK: Second Method - optimized way
 func findAverageSubArrays(arr: [Int], k: Int) -> [Double] {
-    return  []
+    var result = [Double]()
+    
+    var windowSum = 0.0
+    var windowStart = 0
+    
+    for windowEnd in 0...(arr.count - 1) {
+        
+        windowSum += Double(arr[windowEnd]) // add the next element
+        
+        // slide the window , we don't need to slide if we've not hit the required window size of K
+        if windowEnd == k - 1 {
+            result.append(windowSum / Double(k))
+            windowSum -= Double(arr[windowStart])
+            windowStart += 1
+        }
+    }
+    
+    print("Average result:", result, "Second Method")
+    return  result
 }
+
+
+findAverageSubArrays(arr: [1, 3, 2, 6, -1, 4, 1, 8, 2], k: 5)
+
